af3868f2fb69b8e6e705165c48f2f20c0b43840c
ven-gnz <kangas.otso@gmail.com>
2026-05-19 10:53:04 +0300

    Complete chapter tutorial for player animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,13 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D _rb;
     public float movementSpeed;
+    [SerializeField] SpriteRenderer _characterBody;
+    [SerializeField] private Animator _animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _rb = gameObject.GetComponent<Rigidbody2D>();
+        movementSpeed = 3.5f;
     }
 
     // Update is called once per frame
@@ -27,5 +30,11 @@ public class PlayerController : MonoBehaviour
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         movement = Vector2.ClampMagnitude(movement, 1.0f);
         _rb.linearVelocity = movement * movementSpeed;
+
+        bool characterIsWalking = movement.magnitude > 0.0f;
+        _animator.SetBool("isWalking", characterIsWalking);
+
+        bool flipSprite = movement.x < 0.0f;
+        _characterBody.flipX = flipSprite;
     }
 }
