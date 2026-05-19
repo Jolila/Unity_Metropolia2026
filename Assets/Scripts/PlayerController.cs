de5cf9c68f24e0628c1e1183140cd303c78906b8
ven-gnz <kangas.otso@gmail.com>
2026-05-19 12:50:06 +0300

    Chapter 6 Assignment 1 : Death animation implemented (with alpha blending coroutine for a stylistic flair)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 using static UnityEngine.Input;
 
 public class PlayerController : MonoBehaviour
@@ -8,17 +9,40 @@ public class PlayerController : MonoBehaviour
     public float movementSpeed;
     [SerializeField] SpriteRenderer _characterBody;
     [SerializeField] private Animator _animator;
+    Color color;
+    private bool isDead;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _rb = gameObject.GetComponent<Rigidbody2D>();
+        color = _characterBody.color;
         movementSpeed = 3.5f;
+        isDead = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        HandlePlayerMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isDead = true;
+            Debug.Log("space pressed, hero dies");
+            _animator.SetBool("isDead", isDead);
+            StartCoroutine(alphaLerpingFunction(0.5f, 4.5f));
+
+        }
+
+        if (!isDead)
+        {
+            HandlePlayerMovement();
+        }
+        else
+        {
+            _rb.linearVelocity = Vector2.zero;
+           
+        }
+
     }
 
     private void HandlePlayerMovement()
@@ -36,5 +60,26 @@ public class PlayerController : MonoBehaviour
 
         bool flipSprite = movement.x < 0.0f;
         _characterBody.flipX = flipSprite;
+    }
+
+    IEnumerator alphaLerpingFunction(float endValue, float duration)
+    {
+        float time = 0;
+        float startValue = color.a;
+
+        while(time < duration)
+        {
+            
+            time += Time.deltaTime;
+            if(time > 1.0)
+            {
+                color.a = Mathf.Lerp(startValue, endValue, (time - 1.0f) / duration);
+                _characterBody.color = color;
+            }
+            
+            yield return null;
+        }
+        color.a = endValue;
+        _characterBody.color = color;
     }
 }
