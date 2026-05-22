9ce019bc960a551f7fabbbca84f8b857e6a0e77e
ven-gnz <kangas.otso@gmail.com>
2026-05-22 10:50:58 +0300

    Fire rate and shooting skeleton method

--- Assets/Scripts/PlayerStaffController.cs
+++ Assets/Scripts/PlayerStaffController.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 public class PlayerStaffController : MonoBehaviour
 {
 
-
+    [SerializeField] float _fireRate;
+    float _nextFireTime;
     // Update is called once per frame
     void Update()
     {
+        if (Time.timeScale == 0f) return;
         RotateStaff();
-
+        if(Input.GetButton("Fire1") && Time.time >= _nextFireTime)
+        {
+            _nextFireTime = Time.time + 1f / _fireRate;
+            Shoot();
+        }
     }
 
     void RotateStaff()
@@ -19,6 +25,11 @@ public class PlayerStaffController : MonoBehaviour
 
         float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    void Shoot()
+    {
+        Debug.Log("BANGARANGA");
     }
 
 
