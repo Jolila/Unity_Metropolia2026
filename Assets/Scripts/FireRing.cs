1d1e739f1c46ab5940a2607518c8fada639e6955
ven-gnz <kangas.otso@gmail.com>
2026-05-24 19:57:46 +0300

    Fire ring grills rats alive quicklý as intended

--- Assets/Scripts/FireRing.cs
+++ Assets/Scripts/FireRing.cs
@@ -5,6 +5,7 @@ public class FireRing : MonoBehaviour
 
     Transform _playerTransform;
     [SerializeField] float _timer = 2.0f;
+    [SerializeField] float _dps = 10.0f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,6 +17,21 @@ public class FireRing : MonoBehaviour
     void Update()
     {
         transform.position = _playerTransform.position;
+
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Enemy"))
+        {
+            return;
+        }
+
+        if (collision.gameObject.TryGetComponent(out EntityHealth entityHealth))
+        {
+            entityHealth.LoseHealth(Time.fixedDeltaTime * _dps);
+            
+        }
     }
 
     public void InitializeFireAttack(Transform playerTransform)
