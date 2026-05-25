902a1b94cddcbf0a4cbe6511f467e98cc96a42e8
ven-gnz <kangas.otso@gmail.com>
2026-05-25 20:11:24 +0300

    Simple navmesh script for rat horde

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,22 +5,28 @@ public class Enemy : MonoBehaviour
     EntityHealth _entityHealth;
 
     [SerializeField] AudioClip _deathSound;
+
+    UnityEngine.AI.NavMeshAgent _agent;
+    GameObject _target;
     // Start is called once before the first execution of Update after the Mono
     // Behaviour is created
 
     void Awake()
     {
         _entityHealth = GetComponent<EntityHealth>();
+        _agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        _agent.updateRotation = false;
     }
     void Start()
     {
+        _target = GameObject.FindGameObjectWithTag("Player");
         _entityHealth.OnDeath += DestroyEnemy;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _agent.SetDestination(_target.transform.position);
     }
 
     void OnDisable()
