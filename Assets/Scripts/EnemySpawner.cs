89c7d5c96134d242e9d01d8dd6db5f542ed31eb9
ven-gnz <kangas.otso@gmail.com>
2026-05-25 19:44:51 +0300

    Simple enemy spawner with entirely too many rats entirely too fast entirely too far

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField] Tilemap _groundTiles;
+    List<Vector3> _spawnPositions = new();
+
+    [SerializeField] Enemy _enemyPrefab;
+    [SerializeField] float _spawnCooldown;
+    [SerializeField] float _spawnCooldownReductionMultiplier;
+    float _currentCooldown;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SetEnemySpawnPositions();
+        InvokeRepeating(nameof(HandleGameDifficultyIncrease), 1f, 1f);
+    }
+
+    void SetEnemySpawnPositions()
+    {
+        foreach(Vector3Int position in _groundTiles.cellBounds.allPositionsWithin)
+        {
+            if(_groundTiles.HasTile(position))
+            {
+                _spawnPositions.Add(_groundTiles.GetCellCenterWorld(position));
+            }
+        }
+    }
+
+    void HandleGameDifficultyIncrease()
+    {
+        _spawnCooldown *= _spawnCooldownReductionMultiplier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleEnemySpawning();
+    }
+
+    void HandleEnemySpawning()
+    {
+        _currentCooldown -= Time.deltaTime;
+        if (_currentCooldown > Time.time) return;
+
+        _currentCooldown = Time.time + _spawnCooldown;
+        SpawnEnemyToRandomLocation();
+
+    }
+
+    Vector3 GetRandomPosition()
+    {
+        return _spawnPositions[Random.Range(0, _spawnPositions.Count)];
+    }
+
+    void SpawnEnemyToRandomLocation()
+    {
+        Instantiate(_enemyPrefab, GetRandomPosition(), Quaternion.identity);
+    }
+}
