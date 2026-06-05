3b8a4f4bd9cdd56132a81f83ebbae566c5cd69b5
ven-gnz <kangas.otso@gmail.com>
2026-06-05 15:14:49 +0300

    First drawf on some UI stuffs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] InGameUIManager _inGameUIManager;
+    public static GameManager Instance { get; private set; }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] MainMenuManager _mainMenuManager;
+
+    void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(this);
+        }
+        Instance = this;
+        Time.timeScale = 0f;
+    }
+    void Start()
+    {
+        
+    }
+
+    public void ResetGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void StartGame()
+    {
+        Time.timeScale = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale = 0f;
+        _inGameUIManager.ShowGameOverPanel();
+    }
+}
