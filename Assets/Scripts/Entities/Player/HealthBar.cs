a1ae2f41f3bf6ff8d771210bcf1331a2f765f834
ven-gnz <kangas.otso@gmail.com>
2026-08-11 10:36:12 +0300

    Health bar changing first test

--- Assets/Scripts/Entities/Player/HealthBar.cs
+++ Assets/Scripts/Entities/Player/HealthBar.cs
@@ -1,17 +1,39 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+
+public enum HealthBarMode
+{
+    Normal,Underdrive,Overdrive
+}
+
 public class HealthBar : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField] HealthBar _hpBarFill;
+
     [SerializeField] PlayerHealthSystem playerHealth;
 
-  
+
+    [SerializeField] CanvasGroup _normalBar;
+    [SerializeField] CanvasGroup _underdriveBar;
+    [SerializeField] CanvasGroup _overdriveBar;
+
+    [SerializeField] Image _normalHpBarFill;
+    [SerializeField] Image _underdriveHpBarFill;
+    [SerializeField] Image _overdriveHpBarFill;
+
+    HealthBarMode _currentHealtMode;
+
+    private void Awake()
+    {
+        Debug.Log($"NORMAL: {_normalBar}");
+        Debug.Log($"UNDERDRIVE: {_underdriveBar}");
+        Debug.Log($"OVERDRIVE: {_overdriveBar}");
+    }
+
 
     void OnEnable()
     {
-        playerHealth = FindAnyObjectByType<PlayerHealthSystem>();
+        
     }
 
     void OnDisable()
@@ -20,14 +42,74 @@ public class HealthBar : MonoBehaviour
     }
     void Start()
     {
-        RectTransform rt = canvas.GetComponent<RectTransform>();
-        rt.sizeDelta = new Vector2(100, 100);
+        playerHealth = FindAnyObjectByType<PlayerHealthSystem>();
+        Debug.Log($"HealthBar instance: {gameObject.name}");
+        Debug.Log($"NormalBar: {_normalBar}");
+        Debug.Log($"UnderdriveBar: {_underdriveBar}");
+        Debug.Log($"OverdriveBar: {_overdriveBar}");
+        Debug.Log($"PlayerHealth: {playerHealth}");
+
+       
+        _currentHealtMode = HealthBarMode.Normal;
+        SetHealthBarMode(_currentHealtMode);
     }
 
     // Update is called once per frame
     void Update()
     {
-        _hpBarFill.fillAmount = playerHealth.CurrentHealth / playerHealth.CurrentMaxHealth;
+
+        TestInput();
+        UpdateHealthFill();
+        
+
+    }
+
+    private void UpdateHealthFill()
+    {
+        float fillAmount = playerHealth.CurrentHealth / playerHealth.CurrentMaxHealth;
+
+        switch (_currentHealtMode)
+        {
+            case HealthBarMode.Normal:
+                _normalHpBarFill.fillAmount = fillAmount;
+                break;
+
+            case HealthBarMode.Underdrive:
+                _underdriveHpBarFill.fillAmount = fillAmount;
+                break;
+
+            case HealthBarMode.Overdrive:
+                _overdriveHpBarFill.fillAmount = fillAmount;
+                break;
+        }
+    }
+
+    private void TestInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SetHealthBarMode(HealthBarMode.Normal);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SetHealthBarMode(HealthBarMode.Underdrive);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SetHealthBarMode(HealthBarMode.Overdrive);
+        }
+    }
+
+
+    private void SetHealthBarMode(HealthBarMode newMode)
+    {
+        _currentHealtMode = newMode;
+
+        _normalBar.alpha = newMode == HealthBarMode.Normal ? 1f : 0f;
+        _underdriveBar.alpha = newMode == HealthBarMode.Underdrive ? 1f : 0f;
+        _overdriveBar.alpha = newMode == HealthBarMode.Overdrive ? 1f : 0f;
     }
 
    
